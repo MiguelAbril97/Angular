a6c6def614a17db404605ace94589162d88cedd1
Miguel Abril <miguel.abril.alamo.al@iespoligonosur.org>
2025-02-21 22:34:18 +0100

    avanzo coleccion

--- src/app/coleccion/coleccion.component.ts
+++ src/app/coleccion/coleccion.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { SesionService } from '../servicios/sesion.service';
+import { ColectorService } from '../servicios/colector.service';
 
 @Component({
   selector: 'app-coleccion',
@@ -7,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './coleccion.component.css'
 })
 export class ColeccionComponent {
+  
+  public mensaje : String = ''; 
+  public coleccion : Array <number> = []
+  constructor(    
+      protected sesionService: SesionService,
+      private colectorService: ColectorService
+    ) {}
+
+    getColeccion(): void{
+      this.coleccion = this.colectorService.obtenerFavoritos();
+      if(this.sesionService.getCurrentUser()==null){
+        this.mensaje = "Inicie sesion o create una cuenta para añadir juegos aquí"
+      }else if(this.coleccion.length < 1){
+        this.mensaje = "Añade juegos a favoritos para verlos aquí"
+      }else{
+        this.coleccion
+      }
+    }
+
+    ngOnInit(): void{
+      this.getColeccion();
+    }
 
 }
